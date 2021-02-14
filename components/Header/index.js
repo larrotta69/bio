21053a61cbe1654f0b2843a841484995ccd2b4c9
Juan Fernando <ferlar@eltiempo.com>
2021-02-14 13:52:59 -0500

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,11 @@
-import styles from './Header.module.scss'
+import styles from './Header.module.scss';
+import { Button } from '@material-ui/core';
 
 export default function Header() {
 	return (
 		<div className={styles.header}>
 			<h2 className={styles.title}>Header</h2>
+			<Button color="primary">Hello World</Button>
 		</div>
 	)
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/styles.css'
+import '../styles/styles.scss'
+import Head from "next/head";
+
+// Add this wherever you render your code...
 
 export default function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return <>
+		<Head>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+		</Head>
+		<Component {...pageProps} />
+	</>
 }
