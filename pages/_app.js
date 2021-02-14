a5bce6506a94b902817c660e599caa07139b8688
Juan Fernando <ferlar@eltiempo.com>
2021-02-14 15:03:58 -0500

    ajuste header

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import '../styles/styles.scss'
+import '../styles/styles.scss';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Head from "next/head";
 
 // Add this wherever you render your code...
 
 export default function MyApp({ Component, pageProps }) {
 	return <>
+		<CssBaseline />
 		<Head>
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 		</Head>
